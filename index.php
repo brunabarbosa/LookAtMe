5df99b4ccf73006c613ed2c136d498c860e029c2
Abner Matheus <abner.math.c@gmail.com>
2015-10-29 23:51:59 -0200

    First commit

---
+++ index.php
@@ -0,0 +1,90 @@
+<!DOCTYPE>
+<html>
+	<head>
+		<meta charset="utf-8">
+		<meta name="viewport" content="width=device-width, initial-scale=1">
+		<title>Look At Me</title>
+		<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/css/bootstrap.min.css" integrity="sha512-dTfge/zgoMYpP7QbHy4gWMEGsbsdZeCXz7irItjcC3sPUFtf0kuFbDz/ixG7ArTxmDjLXDmezHubeNikyKGVyQ==" crossorigin="anonymous">
+		<link rel="stylesheet" href="css/default.css">
+	</head>
+	<body>
+		<div class="container">
+			<nav class="navbar navbar-default">
+				<div class="container-fluid">
+					<div class="navbar-header">
+						<a class="navbar-brand" href="#"></a>
+					</div>
+				</div>
+			</nav>
+			<div class="panel panel-default">
+				<div class="panel-heading">
+					Últimas perguntas
+				</div>
+				<ul class="list-group">
+					<a href="#" class="list-group-item">
+						<table class="question">
+							<tr>
+								<td>
+									Título da pergunta
+								</td>
+								<td rowspan="2">
+									<span class="badge">10 comentários</span>
+									<span class="glyphicon glyphicon-star star star-active" aria-hidden="true"></span>
+								</td>
+							</tr>
+							<tr>
+								<td>
+									Descrição da pergunta...
+								</td>
+							</tr>
+						</table>
+					</a>
+					<a href="#" class="list-group-item">
+						<table class="question">
+							<tr>
+								<td>
+									Título de outra pergunta
+								</td>
+								<td rowspan="2">
+									<span class="badge">5 comentários</span>
+									<span class="glyphicon glyphicon-star star" aria-hidden="true"></span>
+								</td>
+							</tr>
+							<tr>
+								<td>
+									Descrição da pergunta...
+								</td>
+							</tr>
+						</table>
+					</a>
+					<a href="#" class="list-group-item">
+						<table class="question">
+							<tr>
+								<td>
+									Título de outra pergunta
+								</td>
+								<td rowspan="2">
+									<span class="badge">0 comentários</span>
+									<span class="glyphicon glyphicon-star star" aria-hidden="true"></span>
+								</td>
+							</tr>
+							<tr>
+								<td>
+									Descrição da pergunta...
+								</td>
+							</tr>
+						</table>
+					</a>
+				</ul>
+				<div class="panel-footer">
+					<div class="input-group">
+						<input type="text" class="form-control" placeholder="Digite aqui uma nova pergunta" />
+						<span class="input-group-btn"><button class="btn btn-warning">Enviar</button></span>
+					</div>
+				</div>
+			</div>
+		</div>
+		<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.3/jquery.min.js"></script>
+		<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/js/bootstrap.min.js" integrity="sha512-K1qjQ+NcF2TYO/eI3M6v8EiNYZfA95pQumfvcVrTHtwQVDG+aHRqLi/ETn2uB+1JqwYqVG3LIvdm9lj6imS/pQ==" crossorigin="anonymous"></script>
+	</body>
+</html>
